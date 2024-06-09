97583c15b6dc07b7a871c9a1f0423ee34d196c58
emerg-matters <emerg-matters@gmail.com>
2024-06-09 21:11:17 +0100

    Added all elements to create PWA

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+
+];
+
+//install event
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['site-cache-v1'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
